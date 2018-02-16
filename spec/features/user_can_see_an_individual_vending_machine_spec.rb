71cd99e85642cfea22291d3c7d8cd50fbd14f4f4
Anna Royer <anna.royer@colorado.edu>
2018-02-16 10:13:36 -0700

    Write specs for snacks in vending machine

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
+  before(:each) do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+    @snack = Snack.create(name: 'snickers')
+  end
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
 
-    visit machine_path(dons)
+    visit machine_path(@dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+  end
+
+  scenario 'they see the names of all the snacks associated with that machine and their price' do
+    
   end
 end

b5c1cbb84c3c7b3e56ddf7e1600f4ed3a5aa898d
Anna Royer <anna.royer@colorado.edu>
2018-02-16 10:26:40 -0700

    Add model spec for snack

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-describe Snack, type: :model
+describe Snack, type: :model do
+  context 'relationships' do
+    it { should have_many :machine_snacks }
+    it { should have_many :machines }
+  end
 
+  context 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+  end
 end
